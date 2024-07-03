f9b6e66f915a7597ba75f9f9cc41ffdca1eb9978
FEROZ130405 <160384048+FEROZ130405@users.noreply.github.com>
2024-07-03 14:56:25 +0530

    Add files via upload

--- multi-step-form/src/App.js
+++ multi-step-form/src/App.js
@@ -1,1 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import UserDetails from "./pages/UserDetails";
+import "./App.css";
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <Link to="/"> Home &emsp;</Link>
+          <Link to="/user-details"> User Details </Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/user-details" element={<UserDetails />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
+export default App;
