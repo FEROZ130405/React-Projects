3f275bc8f290ef9bb89ad73feefc006b7d2b8a1b
FEROZ130405 <160384048+FEROZ130405@users.noreply.github.com>
2024-07-03 14:58:05 +0530

    Add files via upload

--- multi-step-form/src/pages/Home.js
+++ multi-step-form/src/pages/Home.js
@@ -1,1 +1,24 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import Step1 from "../components/Step1";
+import Step2 from "../components/Step2";
+import Step3 from "../components/Step3";
+import Summary from "../components/Summary";
+import ProgressBar from "../components/ProgressBar";
+import "../App.css";
 
+const Home = () => {
+  const step = useSelector((state) => state.form.step);
+
+  return (
+    <div>
+      <ProgressBar />
+      {step === 1 && <Step1 />}
+      {step === 2 && <Step2 />}
+      {step === 3 && <Step3 />}
+      {step > 3 && <Summary />}
+    </div>
+  );
+};
+
+export default Home;
---
+++ multi-step-form/src/pages/UserDetails.js
@@ -0,0 +1,12 @@
+import React from "react";
+import UserDetailsTable from "../components/UserDetailsTable";
+
+const UserDetails = () => {
+  return (
+    <div>
+      <UserDetailsTable />
+    </div>
+  );
+};
+
+export default UserDetails;
