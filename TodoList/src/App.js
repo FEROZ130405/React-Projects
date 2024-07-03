6a0413ea4621fa923da9287aaf548c14423a3ae8
FEROZ130405 <160384048+FEROZ130405@users.noreply.github.com>
2024-07-03 14:52:01 +0530

    Add files via upload

--- TodoList/src/App.js
+++ TodoList/src/App.js
@@ -1,1 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import TodoList from './components/TodoList';
+import AddTodo from './components/AddTodo';
+import TodoDetails from './components/TodoDetails';
+import UpdateTodo from './components/UpdateTodo';
+import './App.css';
 
+const App = () => {
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem('todos');
+    return savedTodos ? JSON.parse(savedTodos) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  return (
+    <div className="todo-app">
+      <Router>
+        <Routes>
+          <Route path="/" element={<TodoList todos={todos} setTodos={setTodos} />} />
+          <Route path="/add" element={<AddTodo setTodos={setTodos} />} />
+          <Route path="/details/:id" element={<TodoDetails todos={todos} setTodos={setTodos} />} />
+          <Route path="/update/:id" element={<UpdateTodo todos={todos} setTodos={setTodos} />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
